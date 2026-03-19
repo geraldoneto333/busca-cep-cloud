ee825f518a60d3210a53257e2cf55c063b6e516c
geraldoneto333 <geraldoneto333@hotmail.com>
2026-03-19 00:28:05 +0000

    Adicionado lista de ruas no busca-cep

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-function mostrar(){
+function mostrar() {
 	cep = document.getElementById("cep").value // pegando valor do cep
 	// url = "https://viacep.com.br/ws/"+cep+"/json/" // url do viacep
 	url = `https://viacep.com.br/ws/${cep}/json/` // url do viacep
 
 	// BUSCANDO O CEP USANDO FETCH
 	fetch(url)
-		.then((res)=>{ // variavel "res" irá armazenar a resposta inicial
+		.then((res) => { // variavel "res" irá armazenar a resposta inicial
 			return res.json() // convertendo a resposta em JSON
 		})
-		.then((cep)=>{ // variavel "cep" contendo o json com o CEP do viacep
+		.then((cep) => { // variavel "cep" contendo o json com o CEP do viacep
 			console.log("Oi, meu CEP É no fetch", cep) // imprimindo os dados do cep
 			document.getElementById("cidade").value = cep.localidade
 			document.getElementById("bairro").value = cep.bairro
@@ -18,5 +18,36 @@ function mostrar(){
 		})
 	// FIM DA IMPLEMENTAÇÃO DO FETCH
 	console.log("Oi, meu CEP É fora", cep)
+}
+function mostrarRua() {
+	uf = $("#uf-rua").val()
+	cidade = $("#cidade-rua").val()
+	rua = $("#rua").val()
+	url = `https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/` // url do viacep
+
+	fetch(url)
+		.then((res) => { // variavel "res" irá armazenar a resposta inicial
+			return res.json() // convertendo a resposta em JSON
+		})
+		.then((ruas) => { // variavel "cep" contendo o json com o CEP do viacep
+			console.log("aqui as ruas", ruas)
+
+			let listaRuas = ""
+
+			for (let rua of ruas) {
+				dadosRua = ""
+				const {ddd, ibge, regiao, siafi, ...ruaNova} = rua
+				for (let prop in rua) {
+					dadosRua = dadosRua + `<h6>${rua[prop]}</h6>`
+				}
+				listaRuas = listaRuas + `<li class="collection-item avatar"> ${dadosRua}</li>`
+			}
+
+			document.querySelector("#lista-ruas").innerHTML = listaRuas
+		})
+
+
+	console.log("aqui a url montada", url)
+
 }
 // tag fechamento do script JS
